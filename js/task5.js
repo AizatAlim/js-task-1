5d7b9f501aae6ce2db2926e79b0ada926e7ec4a5
Aizat Alim <aizalimva@gmail.com>
2022-01-12 14:24:52 +0300

    add task5

---
+++ js/task5.js
@@ -0,0 +1,9 @@
+let a = Number(prompt('Введите число a'));
+let b = Number(prompt('Введите число b'));
+console.log(a + b);
+console.log(a - b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+
